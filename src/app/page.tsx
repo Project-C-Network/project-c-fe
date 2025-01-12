48da9781e435777983055efadbb10b3744da828d
gowthamprojectc <gowtham.projectc.network@gmail.com>
2025-01-12 20:28:24 +0530

    chore(commitlint, husky): rules update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LandinPage = () => {
   return (
     <>
-      <h2>Welcome to landing page.. .!</h2>
+      <h2>Welcome to landing pages.. .!</h2>
     </>
   );
 };
